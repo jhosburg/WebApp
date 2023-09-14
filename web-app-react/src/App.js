47dcfd3ac6a86bddcf27b621cb99f708057f180e
Jacob Hosburg <jhosburg22@gmail.com>
2023-09-14 14:40:58 -0700

    Completed React router dom, created home page, and small progress on navbar.

--- web-app-react/src/App.js
+++ web-app-react/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ web-app-react/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from '../components/navbar'
+
+function Home() {
+  return (
+    <div>home</div>
+  )
+}
+
+export default Home
