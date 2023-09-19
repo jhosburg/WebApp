7da3d52429c153303d080846e0d9e6c6e4d9436e
Michael <meng9317@sdsu.edu>
2023-09-19 12:10:37 -0700

    created Home.css and added content to Home.js

--- web-app-react/src/pages/Home.js
+++ web-app-react/src/pages/Home.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import Navbar from '../components/navbar'
+import './Home.css';
 
 function Home() {
   return (
-    <div>home</div>
+    <div class="main">
+            <div class="report-container">
+               <div class="report-header">
+                  <div class="text">
+                     <h1 class="heading">Overall Power Usage</h1>
+                  </div>
+               </div>
+            </div>
+      </div>
   )
 }
 
