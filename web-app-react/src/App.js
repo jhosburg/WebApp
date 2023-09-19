5486fd4a2c26b2d26fe88e1cf8f51889686ac98f
Michael <meng9317@sdsu.edu>
2023-09-19 11:53:51 -0700

    updated 'home' to 'Home' in Home.js

--- web-app-react/src/App.js
+++ web-app-react/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Navbar from './components/navbar';
 function App() {
   return (
