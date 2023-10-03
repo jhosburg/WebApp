0a2191c9b51363e66abc2c1b614ef59c682c2a7e
Jakob Tiger <jtigger7010@gmail.com>
2023-10-03 16:09:20 -0700

    restful api homepage

--- backend/sdei/urls.py
+++ backend/sdei/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('hello/', views.api_hello, name='api_hello'),
     path('json_upload/', views.Json_Upload, name="json_upload"),
     path('grabJson/', views.grab_json, name='grabJson'),
-    path('get_csrf_token/', views.get_csrf_token, name='get_csrf_token'),
 ]
 
 
