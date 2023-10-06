bf6948232aa95b106546e4e23a4c998128a9656f
Moises <mhernandezcast7281@sdsu.edu>
2023-10-05 19:12:48 -0700

    updating mPower files

--- web-app-react/src/pages/mPower/mPower.js
+++ web-app-react/src/pages/mPower/mPower.js
@@ -1,15 +1,15 @@
 import React from 'react';
+import './mPower.css';
 const mPower = () => {
   return (
-    <div style={{ textAlign: 'center' }}>
+    <div className="centered-image-container">
       <img
         src="mPower.png"
         alt="Centered Image"
-        style={{ maxWidth: '100%', height: 'auto' }}
+        className="centered-image"
       />
-      <h1>mPower turns any ordinary circuit breaker panel into a smart panel
-      </h1>
-      <p>Storing energy remains costly, making the ability to squeeze value out
+      <h1>mPower turns any ordinary circuit breaker panel into a smart panel</h1>
+      <p className="centered-text">Storing energy remains costly, making the ability to squeeze value out
          of every kilowatt hour critically important. Here, is where mPower comes in,
          instead of limiting the use of stored energy to a small number of hard-wired
          essential circuits, mPower enables a home or business owner to easily employ 
@@ -19,7 +19,7 @@ const mPower = () => {
         energy storage consumption efficiently and cost-effectively, delivers discretion over
         what circuits to use in any situation, unlocks energy storage performance, and dramatically
         improves its return on investment.
-</p>
+      </p>
     </div>
   );
 };
