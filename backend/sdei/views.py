784a917dff8e66603befd128dcb3f0eb186863e8
Jacob Hosburg <jhosburg22@gmail.com>
2023-10-20 09:54:49 -0700

    Began creating cost function for energy usage, added utilities.py and modified models.py & views.py. Still needs debugging- Jacob

--- backend/sdei/views.py
+++ backend/sdei/views.py
@@ -11,6 +11,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import JsonModel
+from .models import EnergyUsage
+from .utilities import calculate_energy_cost
 from .serializers import JsonModelSerializer
 
 def api_hello(request):
@@ -34,7 +36,22 @@ def grab_json(request):
     
     return JsonResponse(data, safe=False)
 
+def calculate_energy_usage_cost(request):
+    if request.method == 'POST':
+        file_path = 'media/json_data/output.json'
+        with open(file_path, 'r') as json_file:
+            data = request.POST.get('json_file')
+        data = json.loads(json_file)
+        total_cost = 0
 
+        for entry in data:
+            timestamp = entry['timestamp']  # Convert to datetime
+            kwh = entry['kwh']
+            cost = calculate_energy_cost(timestamp, kwh)
+            total_cost += cost
+
+        return JsonResponse({'total_cost': total_cost})
+    return JsonResponse({'error': 'Invalid request'})
 
 
 
