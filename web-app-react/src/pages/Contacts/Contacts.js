02620884cc74032c6f81318d6c882fc36f907359
Moises <mhernandezcast7281@sdsu.edu>
2023-11-03 21:18:52 -0700

    contacts update

--- web-app-react/src/pages/Contacts/Contacts.js
+++ web-app-react/src/pages/Contacts/Contacts.js
@@ -9,32 +9,30 @@ const Contacts = () => {
       <Row>
         <Col lg={6} className="info-section">
           <div className="company-info">
-            <h2>Sea Dragon Understands the Needs of California</h2>
-            
-            <strong>Email:</strong> <a href="mailto:www.seadragon.energy">www.seadragon.energy</a>
+            <h2>Corporate Address</h2>
             <address>
-              <strong>Corporate Address:</strong><br />
               401 Strada Luca<br />
               Florence, TX 76527<br />
               USA
             </address>
+            <strong>Email:</strong> <a href="mailto:www.seadragon.energy">www.seadragon.energy</a>
             <p>
-              We are dedicated to providing innovative solutions for energy management and sustainability.
-            </p>
-            <p>
-              Our mission is to empower individuals and businesses to reduce their energy consumption and carbon footprint.
+              Sea Dragon Energy, Inc. is a majority owned subsidiary of global Air logistics
+              and training, Inc., a veteran-owned small business focused on developing warfighter centric
+              solutions for command, control and communications (C3) systems.
             </p>
           </div>
           <div className="contact-form">
             <h2>Contact Us</h2>
             <Form>
               <Form.Group>
-                <Form.Label>Name</Form.Label>
                 <Form.Control type="text" placeholder="Name" />
               </Form.Group>
               <Form.Group>
-                <Form.Label>Email</Form.Label>
                 <Form.Control type="email" placeholder="Email" />
+              </Form.Group>
+              <Form.Group>
+                <Form.Control type="subject" placeholder="Subject" />
               </Form.Group>
               <Form.Group>
                 <Form.Label>Message</Form.Label>
