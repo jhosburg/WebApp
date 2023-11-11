50fcb517f87fe9c6ea397d0af693db7f8ae3a611
Sandy Solaqa <ssolaqa8226@sdsu.edu>
2023-11-11 14:00:11 -0800

    minor fix on CostOneMonth

--- web-app-react/src/pages/charts/CostOneMonth.js
+++ web-app-react/src/pages/charts/CostOneMonth.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Line } from 'react-chartjs-2';
 import { Chart }            from 'react-chartjs-2'
-import DatePicker from 'react-datepicker';
+
 
 function CostOneMonth({selectedFileName}) {
     const [jsonData, setJsonData] = useState([]);
